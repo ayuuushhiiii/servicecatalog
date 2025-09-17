f3eb4d740d639f35da63e8410c30faf3659fcff7
Ayushi Saxena <Ayushi.Saxena@insight.com>
2025-09-17 17:14:28 +0530

    eleventh commit

--- servicecatalog/main.tf
+++ servicecatalog/main.tf
@@ -12,10 +12,6 @@ provider "aws" {
   region = "us-west-1"
 }
 
-variable "instance_type" { default = "t2.micro" }
-variable "ami_id"        { default = "ami-020cba7c55df1f615" }
-variable "subnet_id"     { default = "subnet-05d699a18f9aa8e91" }
-
 # Service Catalog Portfolio
 resource "aws_servicecatalog_portfolio" "ec2_portfolio" {
   name          = "EC2 Portfolio"
@@ -37,30 +33,11 @@ resource "aws_servicecatalog_product" "ec2_product" {
   }
 }
 
-# Provision EC2 instance from Service Catalog
-resource "aws_servicecatalog_provisioned_product" "ec2_instance" {
-  name                     = "My-EC2-From-ServiceCatalog"
-  product_id               = aws_servicecatalog_product.ec2_product.id
-  # Use the provisioning artifact ID from the AWS console
-  provisioning_artifact_id = "pa-xxxxxxxxxxxx"  
-
-  provisioning_parameters {
-    key   = "InstanceType"
-    value = var.instance_type
-  }
-
-  provisioning_parameters {
-    key   = "AmiId"
-    value = var.ami_id
-  }
-
-  provisioning_parameters {
-    key   = "SubnetId"
-    value = var.subnet_id
-  }
+# Outputs
+output "portfolio_id" {
+  value = aws_servicecatalog_portfolio.ec2_portfolio.id
 }
 
-# Outputs
-output "portfolio_id" { value = aws_servicecatalog_portfolio.ec2_portfolio.id }
-output "product_id"   { value = aws_servicecatalog_product.ec2_product.id }
-output "provisioned_product_id" { value = aws_servicecatalog_provisioned_product.ec2_instance.id }
+output "product_id" {
+  value = aws_servicecatalog_product.ec2_product.id
+}
