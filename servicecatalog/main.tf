8265ff9da5d93e8672feeac62ce69abf31bcf0df
Ayushi Saxena <Ayushi.Saxena@insight.com>
2025-09-17 15:06:09 +0530

    sixth commit

--- servicecatalog/main.tf
+++ servicecatalog/main.tf
@@ -5,13 +5,26 @@ terraform {
       version = ">= 5.0.0"
     }
   }
+  required_version = ">= 1.3"
 }
 
 provider "aws" {
   region = "us-west-1"
 }
 
-# Portfolio
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-020cba7c55df1f615"
+}
+
+variable "subnet_id" {
+  default = "subnet-05d699a18f9aa8e91"
+}
+
+ Portfolio
 resource "aws_servicecatalog_portfolio" "ec2_portfolio" {
   name          = "EC2 Portfolio"
   description   = "Portfolio for EC2 instances"
@@ -32,10 +45,38 @@ resource "aws_servicecatalog_product" "ec2_product" {
   }
 }
 
-# Portfolio-Product Association
-resource "aws_servicecatalog_portfolio_product_association" "association" {
+# Portfolio-Product Association (top-level, not nested)
+resource "aws_servicecatalog_portfolio_product" "association" {
   portfolio_id = aws_servicecatalog_portfolio.ec2_portfolio.id
   product_id   = aws_servicecatalog_product.ec2_product.id
+}
+
+# Data source to fetch artifact ID
+data "aws_servicecatalog_provisioning_artifact" "ec2_artifact" {
+  product_id = aws_servicecatalog_product.ec2_product.id
+  name       = "v1"
+}
+
+# Provision EC2 instance
+resource "aws_servicecatalog_provisioned_product" "ec2_instance" {
+  name                     = "My-EC2-From-ServiceCatalog"
+  product_id               = aws_servicecatalog_product.ec2_product.id
+  provisioning_artifact_id = data.aws_servicecatalog_provisioning_artifact.ec2_artifact.id
+
+  provisioning_parameters {
+    key   = "InstanceType"
+    value = var.instance_type
+  }
+
+  provisioning_parameters {
+    key   = "AmiId"
+    value = var.ami_id
+  }
+
+  provisioning_parameters {
+    key   = "SubnetId"
+    value = var.subnet_id
+  }
 }
 
 # Outputs
@@ -45,4 +86,8 @@ output "portfolio_id" {
 
 output "product_id" {
   value = aws_servicecatalog_product.ec2_product.id
+}
+
+output "provisioned_product_id" {
+  value = aws_servicecatalog_provisioned_product.ec2_instance.id
 }
