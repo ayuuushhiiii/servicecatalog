aecc9d99c6be40edebf40805c7e59a2673093b19
Ayushi Saxena <Ayushi.Saxena@insight.com>
2025-09-17 14:19:55 +0530

    second  commit

--- servicecatalog/main.tf
+++ servicecatalog/main.tf
@@ -1,50 +1,108 @@
+# -------------------------------
+# Provider
+# -------------------------------
+provider "aws" {
+  region = var.region
+}
+
+# -------------------------------
+# Variables
+# -------------------------------
+variable "region" {
+  default = "us-west-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-020cba7c55df1f615" # Replace with valid AMI in your region
+}
+
+variable "subnet_id" {
+  default = "subnet-05d699a18f9aa8e91" # Replace with a subnet in your VPC
+}
+
+# Optional: SSH key
+# variable "key_name" {
+#   default = "your-keypair-name"
+# }
+
+# -------------------------------
+# Portfolio
+# -------------------------------
 resource "aws_servicecatalog_portfolio" "ec2_portfolio" {
   name          = "EC2 Portfolio"
   description   = "Portfolio for EC2 instances"
   provider_name = "MyTeam"
 }
 
+# -------------------------------
+# Product
+# -------------------------------
 resource "aws_servicecatalog_product" "ec2_product" {
   name  = "Simple EC2 Instance"
   owner = "MyTeam"
   type  = "CLOUD_FORMATION_TEMPLATE"
 
-  provisioning_artifact_parameters {
-    name         = "v1"
-    description  = "Initial version"
-    type         = "CLOUD_FORMATION_TEMPLATE"
-    template_url = "https://servicecatalogbucket01.s3.us-west-1.amazonaws.com/ec2-Template.yml"
-  }
+provisioning_artifact_parameters {
+  name         = "v1"
+  description  = "Initial version"
+  type         = "CLOUD_FORMATION_TEMPLATE"
+  template_url = "https://servicecatalogbucket01.s3.us-west-1.amazonaws.com/ec2-Template.yml"
 }
 
-resource "aws_servicecatalog_portfolio_product_association" "association" {
+# -------------------------------
+# Associate Product with Portfolio
+# -------------------------------
+resource "aws_servicecatalog_portfolio_product" "association" {
   portfolio_id = aws_servicecatalog_portfolio.ec2_portfolio.id
   product_id   = aws_servicecatalog_product.ec2_product.id
 }
-# Provision EC2 instance from Service Catalog
+
+# -------------------------------
+# Provision EC2 Instance from Service Catalog
+# -------------------------------
 resource "aws_servicecatalog_provisioned_product" "ec2_instance" {
   name                     = "My-EC2-From-ServiceCatalog"
   product_id               = aws_servicecatalog_product.ec2_product.id
-  provisioning_artifact_id = aws_servicecatalog_product.ec2_product.provisioning_artifact_ids[0]
+  provisioning_artifact_id = aws_servicecatalog_product.ec2_product.provisioning_artifact[0].id
 
   provisioning_parameters {
     key   = "InstanceType"
-    value = "t2.micro"
+    value = var.instance_type
   }
 
   provisioning_parameters {
     key   = "AmiId"
-    value = "ami-020cba7c55df1f615" # Replace with a valid AMI in us-west-1
+    value = var.ami_id
   }
 
   provisioning_parameters {
     key   = "SubnetId"
-    value = "subnet-05d699a18f9aa8e91" # Replace with a subnet ID from your VPC
+    value = var.subnet_id
   }
 
+  # Optional: SSH key
   # provisioning_parameters {
   #   key   = "KeyName"
-  #   value = "your-keypair-name" # Replace with your existing EC2 keypair
+  #   value = var.key_name
   # }
 }
 
+# -------------------------------
+# Outputs
+# -------------------------------
+output "portfolio_id" {
+  value = aws_servicecatalog_portfolio.ec2_portfolio.id
+}
+
+output "product_id" {
+  value = aws_servicecatalog_product.ec2_product.id
+}
+
+output "provisioned_product_id" {
+  value = aws_servicecatalog_provisioned_product.ec2_instance.id
+}
+}
